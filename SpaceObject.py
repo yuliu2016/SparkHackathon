678989827ed27ffe634fa40eec3c377744fe26be
Victor Ma <victor.ma11@icloud.com>
2018-12-15 05:17:42 -0500

    Update SpaceObject.py

--- SpaceObject.py
+++ SpaceObject.py
@@ -5,6 +5,14 @@ import time
 frameRate = 30
 G = 6.6*(10**(-11))
 
+w = 512
+h = 512
+
+pygame.init()
+screen = pygame.display.set_mode([w, h])
+
+clock = pygame.time.Clock()
+
 class SpaceObject:
     vel = [0.0, 0.0] #Velocity, first is x, second is y
     pos = [0.0, 0.0] #position, first is x, second is y
@@ -17,13 +25,13 @@ class SpaceObject:
         self.m = m
         self.r = r
 
-    #def collosion
+    #def collision
 
     def gravForce(self, sO2):
         global G
         global frameRate
-        if(math.abs(self.pos[0] - sO2.pos[0])<0.1 and math.abs(self.pos[1] - sO2.pos[1])<0.1):
-            print("")
+        if(math.sqrt(abs(self.pos[0] - sO2.pos[0])**2 + abs(self.pos[1] - sO2.pos[1])**2) <5):
+            True
         else:
         
             distance = math.sqrt((self.pos[0]-sO2.pos[0])**2+(self.pos[1]-sO2.pos[1])**2) #Calcualte distance
@@ -45,13 +53,11 @@ class SpaceObject:
                     self.vel[0] = (self.vel[0]+math.cos(math.atan((self.pos[1]-sO2.pos[1])/(self.pos[0]-sO2.pos[0])))*force/frameRate/self.m)
                     self.vel[1] = (self.vel[1]+math.sin(math.atan((self.pos[1]-sO2.pos[1])/(self.pos[0]-sO2.pos[0])))*force/frameRate/self.m)
 
-            print(self.vel)
 
     #Updates displacement
     def displacement(self):
         self.pos[0] = self.pos[0]+self.vel[0]/frameRate
         self.pos[1] = self.pos[1]+self.vel[1]/frameRate
-        print(self.pos)
         
 #Class for spaceship
 class SpaceShip(SpaceObject):
@@ -69,30 +75,12 @@ class SpaceShip(SpaceObject):
 kg = float(input("Enter the mass in kg of the spaceship"))
 m = float(input("Enter the radius in meters of the spaceship"))
 
-
-ship = SpaceShip([0,0], [0,0], kg, m, 0, False)
-aD = []
-
-while True:
-    print("Input velocity x, velocity y, position x, position y, mass, and radius of the astroid in that order. Enter a letter to quit.")
-    try:
-        a = float(input())
-        b = float(input())
-        c = float(input())
-        d = float(input())
-        e = float(input())
-        f = float(input())
-    except:
-        break
-
-    aD.append(SpaceObject([a, b],[c, d], e, f))
-
 class SpaceSprite(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.movex = 0
         self.movey = 0
-        img = pygame.image.load('ship.png').convert_alpha()
+        img = pygame.image.load('/Users/victorma/Downloads/ship.png')
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.center = (self.rect.x / 2, self.rect.y / 2)
@@ -108,19 +96,40 @@ class SpaceSprite(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(img, angle)
         self.rectx += self.movex
         self.recty += self.movey
-        
+
+
+ship = SpaceShip([0,0], [0,0], kg, m, 0, False)
+sprite = SpaceSprite (0, 0)
+aD = []
+
+while True:
+    print("Input velocity x, velocity y, position x, position y, mass, and radius of the asteroid in that order. Enter a letter to quit.")
+    try:
+        a = float(input())
+        b = float(input())
+        c = float(input())
+        d = float(input())
+        e = float(input())
+        f = float(input())
+    except:
+        break
+
+    aD.append(SpaceObject([a, b],[c, d], e, f))
+
+
         
         
 while True:
-    for astroid in aD:
-        astroid.displacement()
-        for atroid2 in aD:
-            astroid.gravForce(astroid2)
+    for asteroid in aD:
+        asteroid.displacement()
+        for asteroid2 in aD:
+            asteroid.gravForce(asteroid2)
             
     ship.displacement()
-    for astroid in aD:
-        ship.gravForce(astroid)
+    for asteroid in aD:
+        ship.gravForce(asteroid)
 
     print(ship.pos)
     time.sleep(1/frameRate)
+    pygame.display.flip()
 
